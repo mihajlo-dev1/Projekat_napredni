a2cd8bb74154584ca8f35392f32f5c2ef8d30feb
mihajlo-dev1 <mihajlo.marinkovic005@gmail.com>
2026-02-05 23:51:30 +0100

    wal: add WAL replay for recovery

--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,6 +1,7 @@
 package wal
 
 import (
+	"io"
 	"os"
 	"sync"
 )
@@ -28,6 +29,37 @@ func (w *WAL) Append(record *Record) error {
 	data := record.Serialize()
 	_, err := w.file.Write(data)
 	return err
+}
+
+func (w *WAL) Replay(applyPut func(key, value []byte), applyDelete func(key []byte),
+) error {
+	file, err := os.Open(w.file.Name())
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for {
+		record, err := ReadRecord((file))
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		switch record.Type {
+		case RecordPut:
+			applyPut(record.Key, record.Value)
+
+		case RecordDelete:
+			applyDelete(record.Key)
+		}
+
+	}
+	return nil
+
 }
 
 func (w *WAL) Close() error {
