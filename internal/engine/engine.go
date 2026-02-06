0edc2b67a4393bb34264389e1491b836d160edf8
mihajlo-dev1 <mihajlo.marinkovic005@gmail.com>
2026-02-07 00:06:45 +0100

    engine: use WAL appendPut

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,21 +1,59 @@
 package engine
 
-import "kv-engine/internal"
+import (
+	"kv-engine/internal/memtable"
+	"kv-engine/internal/wal"
+)
 
 type Engine struct {
-	wal      internal.WAL
-	memtable internal.Memtable
-	sstable  internal.SSTable
+	wal      *wal.WAL
+	memtable *memtable.Memtable
 }
 
-func New(
-	wal internal.WAL,
-	memtable internal.Memtable,
-	sstable internal.SSTable,
-) *Engine {
-	return &Engine{
-		wal:      wal,
-		memtable: memtable,
-		sstable:  sstable,
+func New(walPath string) (*Engine, error) {
+	w, err := wal.Open(walPath)
+	if err != nil {
+		return nil, err
+
 	}
+
+	m := memtable.New()
+
+	return &Engine{
+		wal:      w,
+		memtable: m,
+	}, nil
+}
+
+func (e *Engine) Start() error {
+	return e.wal.Replay(
+		func(key []byte, value []byte) {
+			e.memtable.Put(string(key), value)
+		},
+		func(key []byte) {
+			e.memtable.Delete(string(key))
+		},
+	)
+}
+
+func (e *Engine) Put(key string, value []byte) error {
+	if err := e.wal.AppendPut([]byte(key), value); err != nil {
+		return err
+	}
+
+	e.memtable.Put(key, value)
+	return nil
+}
+
+func (e *Engine) Delete(key string) error {
+	if err := e.wal.AppendDelete([]byte(key)); err != nil {
+		return err
+	}
+	e.memtable.Delete(key)
+	return nil
+
+}
+
+func (e *Engine) Get(key string) ([]byte, bool) {
+	return e.memtable.Get(key)
 }
