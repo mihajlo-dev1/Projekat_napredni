c8c4d24790dc367b6e1a286e208b085d486cb1f4
Nikola Radmanovic <nradmanovic42@gmail.com>
2026-04-23 21:34:12 +0200

    Add skiplist

--- internal/skiplist/skiplist.go
+++ internal/skiplist/skiplist.go
@@ -1,23 +1,143 @@
 package skiplist
 
-import "kv-engine/internal"
+import (
+	"math/rand"
+	"time"
 
-type SkipList struct{}
+	"kv-engine/internal"
+)
 
-func New() *SkipList {
-	return &SkipList{}
+type Node struct {
+	key     string
+	value   []byte
+	deleted bool
+	forward []*Node
 }
 
-func (s *SkipList) Put(key string, value []byte) {}
+type SkipList struct {
+	head     *Node
+	level    int
+	maxLevel int
+	size     int
+	rng      *rand.Rand
+}
+
+func New() *SkipList {
+	const defaultMaxLevel = 16
+
+	head := &Node{
+		forward: make([]*Node, defaultMaxLevel),
+	}
+
+	return &SkipList{
+		head:     head,
+		level:    1,
+		maxLevel: defaultMaxLevel,
+		size:     0,
+		rng:      rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+func (s *SkipList) Put(key string, value []byte) {
+	update := make([]*Node, s.maxLevel)
+	current := s.head
+
+	for level := s.level - 1; level >= 0; level-- {
+		for current.forward[level] != nil && current.forward[level].key < key {
+			current = current.forward[level]
+		}
+		update[level] = current
+	}
+
+	current = current.forward[0]
+	if current != nil && current.key == key {
+		current.value = append([]byte(nil), value...)
+		current.deleted = false
+		return
+	}
+
+	nodeLevel := s.randomLevel()
+	if nodeLevel > s.level {
+		for level := s.level; level < nodeLevel; level++ {
+			update[level] = s.head
+		}
+		s.level = nodeLevel
+	}
+
+	newNode := &Node{
+		key:     key,
+		value:   append([]byte(nil), value...),
+		deleted: false,
+		forward: make([]*Node, nodeLevel),
+	}
+
+	for level := 0; level < nodeLevel; level++ {
+		newNode.forward[level] = update[level].forward[level]
+		update[level].forward[level] = newNode
+	}
+
+	s.size++
+}
+
+func (s *SkipList) randomLevel() int {
+	level := 1
+	for level < s.maxLevel && s.rng.Intn(2) == 0 {
+		level++
+	}
+	return level
+}
 
 func (s *SkipList) Get(key string) ([]byte, bool) {
+	current := s.head
+
+	for level := s.level - 1; level >= 0; level-- {
+		for current.forward[level] != nil && current.forward[level].key < key {
+			current = current.forward[level]
+		}
+	}
+
+	current = current.forward[0]
+	if current != nil && current.key == key && !current.deleted {
+		return append([]byte(nil), current.value...), true
+	}
+
 	return nil, false
 }
 
 func (s *SkipList) Delete(key string) bool {
+	current := s.head
+
+	for level := s.level - 1; level >= 0; level-- {
+		for current.forward[level] != nil && current.forward[level].key < key {
+			current = current.forward[level]
+		}
+	}
+
+	current = current.forward[0]
+	if current != nil && current.key == key && !current.deleted {
+		current.deleted = true
+		current.value = nil
+		return true
+	}
+
 	return false
 }
 
 func (s *SkipList) Entries() map[string]internal.MemtableEntry {
-	return nil
+	result := make(map[string]internal.MemtableEntry, s.size)
+	current := s.head.forward[0]
+
+	for current != nil {
+		entry := internal.MemtableEntry{
+			Deleted: current.deleted,
+		}
+		if current.value != nil {
+			entry.Value = append([]byte(nil), current.value...)
+		}
+
+		result[current.key] = entry
+		current = current.forward[0]
+	}
+
+	return result
 }
