ecd8131fe6e46d522962253184a0c2de8e14891c
Nikola Radmanovic <nradmanovic42@gmail.com>
2026-04-23 11:50:30 +0200

    Add configuration file

--- cmd/kvengine/main.go
+++ cmd/kvengine/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"kv-engine/internal/config"
+)
 
 func main() {
-	fmt.Println("project starting")
+	cfg, err := config.Load("config.json")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Printf(
+		"project starting with memtable=%s, walBlocks=%d, blockSizeKB=%d\n",
+		cfg.Memtable.Implementation,
+		cfg.WAL.SegmentSizeBlocks,
+		cfg.BlockManager.BlockSizeKB,
+	)
 }
